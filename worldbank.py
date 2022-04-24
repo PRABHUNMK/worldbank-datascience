a8a92cc6cb0e71e285e63976aec3035e94fe74f0
prabhusadasivam <97303592+PRABHUNMK@users.noreply.github.com>
2022-04-24 13:31:41 +0530

    Add files via upload

---
+++ worldbank.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as py
+import altair as alt
+import streamlit as st
+
+st.title('World Bank Data-India')
+
+India=pd.read_csv('World_Bank_India.csv',skiprows=4)
+st.write(India)
